01927768c5a5b13aa22e73052d0ba0170bb6e8b5
NiklasMickelat <niklas.mickelat@web.de>
2023-04-02 17:50:44 +0200

    add a car Class

--- main.js
+++ main.js
@@ -1,6 +1,48 @@
-const canvas = document.getElementById("carCanvas");
+//#region Gobals
 
-canvas.height = window.innerHeight;
-canvas.width = 200;
+const CANVAS = document.getElementById("carCanvas");
+CANVAS.height = window.innerHeight;
+CANVAS.width = 200;
 
-const ctx = canvas.getContext("2d");
+//#endregionGlobals
+
+
+
+//#region Klassen
+
+class Car {
+    constructor(x, y ,width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    draw(ctx){
+        ctx.beginPath();
+        ctx.rect(this.x - this.width/2, this.y-this.height/2, this.width, this.height);
+        ctx.fill();
+    }
+}
+
+//#endregion Klassen
+
+
+
+//#region Funktionen
+//#endregion Funktionen
+
+
+
+//#region Main
+
+const ctx = CANVAS.getContext("2d");
+auto = new Car(CANVAS.width/2, CANVAS.height/2, 50, 75);
+auto.draw(ctx);
+
+//#endregion Main
+
+
+
+//#region EventListener
+//#endregion EventListener
